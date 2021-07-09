df3ed8cbdccfda42bb577a0591d18c142900b321
asmit-choudhary <choudharyrorasmit@gmail.com>
2021-07-09 19:52:41 +0530

    Add number keys

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-document.querySelector('#code65').addEventListener("onkeydown", keyPressed);
-document.querySelector('#code65').addEventListener("onkeyup", keyRelease);
 
+document.addEventListener("keydown", keyPressed);
+document.addEventListener("keyup", keyRelease);
 
+const audioControls = document.createElement("audio");
 
-for(let i = 65; i <= 90; i++){
-    let elem = "#code" + i;
-    console.log(document.querySelector(elem));
-    console.log(elem);
+function play(source){
+    audioControls.setAttribute("src", source);
+    audioControls.load();
+    audioControls.play();
 }
 
-function keyPressed(){
+function keyPressed(evt){
     evt = evt || window.event;
-    document.querySelector("#code"+evt.keyCode).style.boxShadow = "5px 5px 6px #20242a,-5px -5px 6px #30343e";
-    console.log(evt.keyCode);
+    if((evt.keyCode > 47 && evt.keyCode < 58) || (evt.keyCode > 64 && evt.keyCode < 91)){
+        document.querySelector("#code"+evt.keyCode).style.boxShadow = "5px 5px 6px #20242a,-5px -5px 6px #30343e";
+        play(NOTES[evt.keyCode].noteName);
+        console.log(evt.keyCode);
+    }
 }
 
 function keyRelease(evt){
     evt = evt || window.event;
-    document.querySelector("#code"+evt.keyCode).style.boxShadow = "5px 5px 9px #101215, -5px -5px 9px #404653";
-    console.log(evt.keyCode);
+    if((evt.keyCode > 47 && evt.keyCode < 58) || (evt.keyCode > 64 && evt.keyCode < 91)){
+        document.querySelector("#code"+evt.keyCode).style.boxShadow = "5px 5px 9px #101215, -5px -5px 9px #404653";
+        console.log(evt.keyCode);
+    }
 }
